0fdb3ad9e7f5bfd493d465aea8f0e5acd3c08193
viralsavla3 <41993740+viralsavla3@users.noreply.github.com>
2018-08-04 17:43:56 +0530

    commit

---
+++ JDBCProject/src/ResultDemo.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+public class ResultDemo {
+	public static void main(String[] args) {
+		Connection con = null;
+		String sql = "select * from product";
+
+		try {
+			con = JDBCUtil.getConnection();
+			ResultSet rs = con.createStatement().executeQuery(sql);
+			ResultSetMetaData meta = rs.getMetaData();
+			for (int i = 1; i <= meta.getColumnCount(); i++) 
+				System.out.print(meta.getColumnName(i) + "\t");
+			System.out.println();
+
+			while (rs.next())
+				System.out.printf("%5d\t%-15s\t%5s\n", rs.getInt(1), rs.getString(2) ,rs.getString(3));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
